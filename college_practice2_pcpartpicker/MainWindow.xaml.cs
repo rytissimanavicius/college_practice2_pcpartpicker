7051496cd2b234e3d00a4ab8445da5320661a19e
Rytis Simanavičius <rytucis.qua@gmail.com>
2019-11-30 05:00:26 +0200

    Quick change: dabar galima ir pašalinti dalykus iš pasirinkimų sąrašo, biškį DB pasipilde, pažaidžiau su stiliais.

--- college_practice2_pcpartpicker/MainWindow.xaml.cs
+++ college_practice2_pcpartpicker/MainWindow.xaml.cs
@@ -13,28 +13,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-//TODO:
-//- pervadinti puse pavadinimu nes jie kvaili
-//- padaryt sioki toki prisijungima
-//- leisti papildyti/editinti db
-//- tikrinti pasirinktu komponentu suderinamuma
-//- kainas ideti
-
 namespace college_practice2_pcpartpicker
 {
     public partial class MainWindow : Window
     {
-        //listas objektu pasirinktu daliu informacijos datagridui
-        List<PasirinktosDalys> PasirinktosDalys = new List<PasirinktosDalys>();
-        //db ir path
+        //sitam liste laikoma informacija kuri bus atvaizduoja pasirinktu daliu datagride
+        List<PasirinktosDalys> PasirinktosDalysList = new List<PasirinktosDalys>();
+        //kurti db, path per konstruktoriu
         Database DB = new Database(@"Data Source=C:\Users\rytuciss\source\repos\college_practice2_pcpartpicker\college_practice2_pcpartpicker\database\database.db");
         public MainWindow()
         {
             InitializeComponent();
-            //setupas
+            //inicializavimas
             try
             {
-                //is db imama info ir kuriami objektai
+                //is db imama informacija ir kuriami objektai
                 DB.SukurtiObjektus();
                 //sukurus db sukuriamas listas stringu, kuriame laikoma kategorijos lengvai prieigai
                 for (int i = 0; i < DB.GetKategorijuList().Count; i++)
@@ -43,9 +36,9 @@ namespace college_practice2_pcpartpicker
                     PasirinktiKategorija.Items.Add(DB.GetKategorijuList()[i]);
                     //sukuriami objektai pasirinktu daliu ir duodamos kategorijos,
                     //kita informacija bus sudedama tik pridejus konkrecias dalis
-                    PasirinktosDalys.Add(new PasirinktosDalys(DB.GetKategorijuList()[i]));
+                    PasirinktosDalysList.Add(new PasirinktosDalys(DB.GetKategorijuList()[i]));
                     //atvaizduojama pasirinktos dalys datagride
-                    PasirinktosDalysSarasas.Items.Add(PasirinktosDalys[i]);
+                    PasirinktosDalysSarasas.Items.Add(PasirinktosDalysList[i]);
                 }
                 //kad nebutu tuscias combobox paleidus programa, pasirenka pirma kategorija
                 PasirinktiKategorija.SelectedItem = DB.GetKategorijuList()[0];
@@ -58,80 +51,84 @@ namespace college_practice2_pcpartpicker
         }
         private void PasirinktiKategorijaComboBox(object sender, SelectionChangedEventArgs e)
         {
-            DaliuPasirinkimas.Items.Clear();
+            DaliuPasirinkimasSarasas.Items.Clear();
             //atsizvelgiama kokia kategorija pasirinkta kategorijos combobox ir supildo sarasa
             if (PasirinktiKategorija.SelectedItem.ToString() == "COOLER")
                 for (int i = 0; i < DB.GetCoolerList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetCoolerList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetCoolerList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "CPU")
                 for (int i = 0; i < DB.GetCPUList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetCPUList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetCPUList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "GPU")
                 for (int i = 0; i < DB.GetGPUList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetGPUList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetGPUList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "HDD")
                 for (int i = 0; i < DB.GetHDDList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetHDDList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetHDDList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "KORPUSAS")
                 for (int i = 0; i < DB.GetKorpusuList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetKorpusuList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetKorpusuList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "MOBO")
                 for (int i = 0; i < DB.GetMOBOList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetMOBOList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetMOBOList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "PSU")
                 for (int i = 0; i < DB.GetPSUList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetPSUList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetPSUList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "RAM")
                 for (int i = 0; i < DB.GetRAMList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetRAMList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetRAMList()[i]);
             else if (PasirinktiKategorija.SelectedItem.ToString() == "SSD")
                 for (int i = 0; i < DB.GetSSDList().Count; i++)
-                    DaliuPasirinkimas.Items.Add(DB.GetSSDList()[i]);
+                    DaliuPasirinkimasSarasas.Items.Add(DB.GetSSDList()[i]);
         }
         private void DuotiDaliPasirinktiems(object sender, RoutedEventArgs e)
         {
-            //paspaudus prideti dali, isrenkama informacija buvusi toje eilute
-            DataGridCellInfo GamintojoCell = DaliuPasirinkimas.SelectedCells[1];
+            DataGridCellInfo GamintojoCell = DaliuPasirinkimasSarasas.SelectedCells[1];
             string Gamintojas = (GamintojoCell.Column.GetCellContent(GamintojoCell.Item) as TextBlock).Text;
-            DataGridCellInfo ModelioCell = DaliuPasirinkimas.SelectedCells[2];
+            DataGridCellInfo ModelioCell = DaliuPasirinkimasSarasas.SelectedCells[2];
             string Modelis = (ModelioCell.Column.GetCellContent(ModelioCell.Item) as TextBlock).Text;
-            DataGridCellInfo SpecifikacijosCell = DaliuPasirinkimas.SelectedCells[3];
+            DataGridCellInfo SpecifikacijosCell = DaliuPasirinkimasSarasas.SelectedCells[3];
             string Specifikacija = (SpecifikacijosCell.Column.GetCellContent(SpecifikacijosCell.Item) as TextBlock).Text;
 
             PasirinktosDalysSarasas.Items.Clear();
 
-            //pasiziurima koke kategorija pasirinkta combobox, 
-            //einama i lista pasirinktu daliu ir ieskoma sutampancios kategorijos, 
-            //tada virsuje isrinkta informacija sudedama i atitinkama objekta
-            for (int i = 0; i < PasirinktosDalys.Count; i++)
+            for (int i = 0; i < PasirinktosDalysList.Count; i++)
             {
-                if (PasirinktosDalys[i].GetKategorija() == PasirinktiKategorija.Text)
+                if (PasirinktosDalysList[i].GetKategorija() == PasirinktiKategorija.Text)
                 {
-                    PasirinktosDalys[i].Gamintojas = Gamintojas;
-                    PasirinktosDalys[i].Modelis = Modelis;
-                    PasirinktosDalys[i].Specifikacija = Specifikacija;
+                    PasirinktosDalysList[i].Gamintojas = Gamintojas;
+                    PasirinktosDalysList[i].Modelis = Modelis;
+                    PasirinktosDalysList[i].Specifikacija = Specifikacija;
                 }
-                PasirinktosDalysSarasas.Items.Add(PasirinktosDalys[i]);
+                PasirinktosDalysSarasas.Items.Add(PasirinktosDalysList[i]);
             }
         }
         private void PasalintiPasirinktaDali(object sender, RoutedEventArgs e)
         {
+            DataGridCellInfo ModelioCell = PasirinktosDalysSarasas.SelectedCells[2];
+            string Modelis = (ModelioCell.Column.GetCellContent(ModelioCell.Item) as TextBlock).Text;
 
+            PasirinktosDalysSarasas.Items.Clear();
+
+            for (int i = 0; i < PasirinktosDalysList.Count; i++)
+            {
+                if (PasirinktosDalysList[i].Modelis == Modelis)
+                {
+                    PasirinktosDalysList[i].Gamintojas = null;
+                    PasirinktosDalysList[i].Modelis = null;
+                    PasirinktosDalysList[i].Specifikacija = null;
+                }
+                PasirinktosDalysSarasas.Items.Add(PasirinktosDalysList[i]);
+            }
         }
-
-
-
-
-
-
         public void SkaiciuotiGalia()
         {
             int ReikalingaGalia = 0;
 
-            for (int i = 0; i < PasirinktosDalys.Count; i++)
-                if (PasirinktosDalys[i].GetKategorija() == "CPU")
+            for (int i = 0; i < PasirinktosDalysList.Count; i++)
+                if (PasirinktosDalysList[i].GetKategorija() == "CPU")
                     for (int j = 0; j < DB.GetCPUList().Count; j++)
-                        if (DB.GetCPUList()[i].GetModelis() == PasirinktosDalys[i].Modelis)
+                        if (DB.GetCPUList()[i].GetModelis() == PasirinktosDalysList[i].Modelis)
                             ReikalingaGalia += DB.GetCPUList()[i].GetGaliosReikalavimai();
             
             GaliosReikalavimai.Content = ReikalingaGalia;
@@ -144,8 +141,8 @@ namespace college_practice2_pcpartpicker
         public void TikrintiSuderinamuma()
         {
             //cooler 1 tikrinimas su mobo 
-            int i = PasirinktosDalys.FindIndex(a => a.GetKategorija() == "COOLER");
-            int j = DB.GetCoolerList().FindIndex(a => a.GetModelis() == PasirinktosDalys[i].Modelis);
+            int i = PasirinktosDalysList.FindIndex(a => a.GetKategorija() == "COOLER");
+            int j = DB.GetCoolerList().FindIndex(a => a.GetModelis() == PasirinktosDalysList[i].Modelis);
             //string UncutString = DB.GetCoolerList()[j].GetJungtiesTipas();
             //char[] temp = UncutString.ToCharArray();
             //for (int k = 0; )
